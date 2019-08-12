6042ff6cb9e11c45708e7d5fe924ca5dbff39015
srfngrc <srfngrc@gmail.com>
2019-08-12 19:12:03 -0400

    i add by hardcode the files related with WebForms.master 7h11pm

--- WebForms.Master.designer.cs
+++ WebForms.Master.designer.cs
@@ -7,36 +7,36 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WebFormsApplicationSERAFIN {
+namespace RentMyWrox {
     
     
-    public partial class SiteMaster {
+    public partial class WebForms {
         
         /// <summary>
-        /// LeftNavigation control.
+        /// head control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu LeftNavigation;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// SiteMapDataSourceSRFN control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteMapDataSourceSRFN;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// MainContent control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
